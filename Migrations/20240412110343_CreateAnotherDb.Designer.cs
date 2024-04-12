ce56b2f53d1a2dde8579727a0fae0f4038e706f8
Anatolii Kupriienko <takblet7331228322@gmail.com>
2024-04-12 14:08:38 +0300

    Fixed DB, added db backup file and readme

--- Migrations/20240412110343_CreateAnotherDb.Designer.cs
+++ Migrations/20240412110343_CreateAnotherDb.Designer.cs
@@ -4,6 +4,7 @@ using BitsOrchestraTest.Db;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BitsOrchestraTest.Migrations
 {
     [DbContext(typeof(PersonDbContext))]
-    partial class PersonDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240412110343_CreateAnotherDb")]
+    partial class CreateAnotherDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ README.md
@@ -0,0 +1,4 @@
+### To edit a field, double click the value. 
+### Click somewhere else after inputing new data to save
+
+This task took me 4-5 hours spread over 2 days.
